cc2d95125ad89db8c4884a84ccaf51366534cb7c
lynntlc <lynn050179@gmail.com>
2022-11-19 23:39:35 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI)
+
 const port = 3000
-const exphbs = require('express-handlebars')
+// const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+// app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 
 app.set('view engine', 'handlebars')
 
