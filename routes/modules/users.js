f1643367ef97ca631d5ac3363334038272283176
lynntlc <lynn050179@gmail.com>
2023-02-11 23:48:59 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -36,5 +36,9 @@ router.post('/register', (req, res) => {
   })
     .catch(err => console.log(err))
 })
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
 
 module.exports = router
