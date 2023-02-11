5fdbc01047d7bfddfc43887cfe427cc12c4110f0
lynntlc <lynn050179@gmail.com>
2023-02-11 21:03:27 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-// 載入首頁路由
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 module.exports = router
