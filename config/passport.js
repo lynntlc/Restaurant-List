9ec34a5adf61d7d921e722ed121e529a2e88f4ac
lynntlc <lynn050179@gmail.com>
2023-02-12 21:32:24 +0800

    modify login warning message

--- config/passport.js
+++ config/passport.js
@@ -3,19 +3,20 @@ const LocalStrategy = require('passport-local').Strategy
 const FacebookStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
 const User = require('../models/user')
+const flash = require('connect-flash')
 
 module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: 'That email is not registered!' })
+          return done(null, false, req.flash('warning_msg', '此電子信箱尚未註冊'))
         }
         return bcrypt.compare(password, user.password).then(isMatch => {
           if (!isMatch) {
-            return done(null, false, { message: 'Email or Password incorrect.' })
+            return done(null, false, req.flash('warning_msg', '電子信箱或密碼錯誤'))
           }
           return done(null, user)
         })      
