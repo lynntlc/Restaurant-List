82126ed8a77b5b0b8eaf61b06d1b7249c4465a22
lynntlc <lynn050179@gmail.com>
2023-02-12 12:50:21 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -15,29 +15,44 @@ router.get('/register', (req, res) => {
 })
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: 'Email與密碼為必填欄位。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '請確認你的密碼是否正確。' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ email }).then(user => {
     if (user) {
-      console.log('User already exists.')
-      res.render('register', {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      return res.render('register', {
+        errors,
         name,
         email,
         password,
         confirmPassword
       })
-    } else {
-      return User.create({
+    } return User.create({
         name,
         email,
         password
       })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
-    }
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+    })
   })
-    .catch(err => console.log(err))
-})
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
